1702e596444f1b36a6b30026e923400fd4010072
SoVa-web <54186792+SoVa-web@users.noreply.github.com>
2020-06-07 15:01:33 +0300

    Function for distance

--- RTree.cpp
+++ RTree.cpp
@@ -37,3 +37,22 @@ void RTree::readFile(std::string fileName)
 		std::cout << "File wasn't opened" << std::endl;
 	}
 }
+
+
+double RTree::distance(double A1, double B1, double A2, double B2){
+double result, a1, b1, a2, b2, delta1, delta2;
+double radius = 6367444.65;//radius in meters
+//latitude and longitude in radians
+a1 = (A1*2*acos(0.0))/180;
+b1 = (B1*2*acos(0.0))/180;
+a2 = (A2*2*acos(0.0))/180;
+b2 = (B2*2*acos(0.0))/180;
+
+delta1 = a2 - a1;
+delta2 = b2 - b1;
+
+double squereRoot = sqrt(pow(sin(delta1/2), 2)+cos(a2)*cos(a1)*pow(sin(delta2/2), 2));
+result = 2*asin(squereRoot)*radius;
+
+return result;
+}
--- RTree.h
+++ RTree.h
@@ -2,6 +2,7 @@
 
 #include <fstream>
 #include <iostream>
+#include <cmath>
 
 #include "Node.h"
 
@@ -10,6 +11,7 @@ class RTree
 public:
 	RTree();
 	void readFile(std::string fileName);
+	double distance(double A1, double B1, double A2, double B2);
 
 private:
 	Node* root;
